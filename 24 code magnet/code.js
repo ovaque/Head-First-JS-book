e59755c5cff5a7ea7d1392ab2a88f099cbf2b170
ovaque <ovaque@outlook.com>
2018-02-14 12:47:12 +0530

    All codes from Head First Book

---
+++ 24 code magnet/code.js
@@ -0,0 +1,8 @@
+var i=0;
+var name="joe";
+while(i<2){
+    document.write("Happy Birthday to you.<br>");
+    i=i+1;
+    }
+    document.write("Happy Birthday dear "+ name +",<br>");
+    document.write("Happy Birthday to you.<br>");
